f01a169e6904c32cbea6620dd7031e27816bca0a
kalpana <kalpanapattajava@gmail.com>
2022-09-25 18:04:31 +0530

    Revert "this is my new project"
    This reverts commit f410b153b51c9be31efeac499e875f0dff582141.

--- src/main/java/com/org/capgemini/service/BoyFriendService.java
+++
@@ -1,9 +0,0 @@
-package com.org.capgemini.service;
-
-import com.org.capgemini.model.BoyFriends;
-import org.springframework.web.bind.annotation.RequestBody;
-
-public interface BoyFriendService {
-    public void addBoyFriend( BoyFriends boyfriends);
-
-}
